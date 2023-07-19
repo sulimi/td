49386c8c332785aeec4e4ac661cc0fc4765f9972
sumi <1102523711@qq.com>
2023-07-19 16:27:28 +0800

    update

---
+++ assets/remove-loading.888bfe71.js
@@ -0,0 +1,1 @@
+const o=()=>{const e=document.querySelector("#load-page");e==null||e.remove()};export{o as r};
---
+++ assets/scrollRefHook.df38fe7d.js
@@ -0,0 +1,1 @@
+function c(){const o=document.querySelector("#app");return{resetScroll:()=>{o==null||o.scrollTo(0,0)}}}export{c as u};
